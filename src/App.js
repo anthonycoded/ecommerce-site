386bf39ece59ebd81200dd8a495d7515002ee7d8
anthonycoded <barbels33@gmail.com>
2022-06-07 21:22:17 -0400

    added tailwind

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import AppWrapper from "./AppWrapper";
+import { AuthProvider } from "./utils/authFunctions";
+import "./App.css";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <AuthProvider>
+        <AppWrapper></AppWrapper>
+      </AuthProvider>
+    </Provider>
+  );
+}
+
+export default App;
