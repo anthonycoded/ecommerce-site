0bf033f49fe32bedf802243d61d11088e8d4fb1b
anthonycoded <barbels33@gmail.com>
2022-06-07 21:21:19 -0400

    added postcss for tailwind

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+//const tailwindcss = require("tailwindcss");
+//const purgeCSS = require("@fullhuman/postcss-purgecss");
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
