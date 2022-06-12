ee8b97fde2ff881ddfba33bb5ab2671b284759e5
anthonycoded <barbels33@gmail.com>
2022-06-11 21:27:36 -0400

    added reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { customerReducer } from "./reducers/customerReducer";
 import { cartReducer } from "./reducers/cartReducer";
 import { authReducer } from "./reducers/authReducer";
+import { productReducer } from "./reducers/productReducer";
 
 export const store = configureStore({
-  reducer: { auth: authReducer, customers: customerReducer, cart: cartReducer },
+  reducer: {
+    auth: authReducer,
+    customers: customerReducer,
+    cart: cartReducer,
+    products: productReducer,
+  },
 });
