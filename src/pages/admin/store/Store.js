3e78e7943491f3f89bc0115e66c3bf69a27ef098
anthonycoded <barbels33@gmail.com>
2022-06-23 13:11:55 -0400

    updated styles, updated layout

--- src/pages/admin/store/Store.js
+++ src/pages/admin/store/Store.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect, useRef, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { GetProducts } from "../../../store/actions/productActions";
 import Upload from "./Upload/Upload";
-import right from "../../../assets/right.png";
-import Song from "./Song";
-import { GetStore } from "../../../store/actions/songActions";
+import right from "../../../../assets/right.png";
+import ProductCard from "../../../components/cards/ProductCard";
 import Pagination from "../../../components/UI/Pagination/Pagination";
 
 const Store = () => {
   const [showUpload, setShowUpload] = useState(false);
-  const state = useSelector((state) => state.store);
-  const [products, setProducts] = useState();
+  const state = useSelector((state) => state.products);
+  const [products, setProducts] = useState([]);
   const dispatch = useDispatch();
 
   function toggleUpload() {
@@ -18,7 +18,7 @@ const Store = () => {
   }
 
   function ResfreshList() {
-    dispatch(GetSongs());
+    dispatch(GetProducts());
   }
   //Pagination Logic
   const [currentPage, setCurrentPage] = useState(1);
@@ -28,20 +28,20 @@ const Store = () => {
   const currentTableData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
-    return songs.slice(firstPageIndex, lastPageIndex);
+    return products?.slice(firstPageIndex, lastPageIndex);
   }, [currentPage]);
 
   useEffect(() => {
-    if (state.store?.products?.length > 0) {
+    if (state.products?.length > 0) {
       setProducts(state.store?.products);
     }
   }, [state]);
 
   return (
-    <div className=" py-8">
-      <div className="w-full h-full flex flex-col items-start  px-4">
+    <div className=" py-8 w-full flex flex-col items-center justify-start">
+      <div className="w-full lg:w-5/6 h-full flex flex-col items-start  px-4">
         <p className="text-3xl  w-full ">Manage Store</p>
-        <div className="flex w-full h-full justify-start my-2 space-x-2 text-md items-center">
+        <div className="flex w-full h-full justify-start my-1 space-x-2 text-md items-center px-2">
           <Link to="/admin" className=" font-semibold underline ">
             Admin
           </Link>
@@ -49,7 +49,7 @@ const Store = () => {
           <p>Store</p>
         </div>
       </div>
-      <div className="px-4">
+      <div className="px-4 w-full lg:w-5/6 mb-12 lg:mb-24">
         <button
           className="bg-blue-300 px-4 py-2 rounded-lg shadow-xl "
           onClick={toggleUpload}
@@ -61,22 +61,31 @@ const Store = () => {
         </button>
       </div>
       {showUpload ? <Upload setOpen={setShowUpload}></Upload> : undefined}
-      <div className="pt-12 w-full flex flex-col items-center ">
-        <div className="w-full sm:w-4/5 lg:w-3/5">
+      <div className="w-full flex flex-col items-center ">
+        <div className="w-full sm:w-4/5 lg:w-3/5 ">
           <p className="text-2xl font-semibold w-full text-left px-2 mb-4">
             Products in store
           </p>
           <div className="px-4 w-full lg:h-050 flex flex-col items-start justify-start ">
-            {currentTableData?.map((item, i) => (
-              <Song item={item} index={i} key={i}></Song>
-            ))}
-            <Pagination
-              className="pagination-bar"
-              currentPage={currentPage}
-              totalCount={products?.length}
-              pageSize={PageSize}
-              onPageChange={(page) => setCurrentPage(page)}
-            />
+            {products.length > 0 ? (
+              <>
+                {currentTableData?.map((item, i) => (
+                  <ProductCard item={item} index={i} key={i}></ProductCard>
+                ))}
+                <Pagination
+                  className="pagination-bar"
+                  currentPage={currentPage}
+                  totalCount={products?.length}
+                  pageSize={PageSize}
+                  onPageChange={(page) => setCurrentPage(page)}
+                />
+              </>
+            ) : (
+              <>
+                <p>You do not have any products for sale.</p>
+                <p>You can upload products and start selling at any time.</p>
+              </>
+            )}
           </div>
         </div>
       </div>
