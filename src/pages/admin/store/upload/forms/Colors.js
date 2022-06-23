ab5b0ca716088d8db099f793bb29124e382dcd12
anthonycoded <barbels33@gmail.com>
2022-06-23 13:12:45 -0400

    updated sizes and quantities

--- src/pages/admin/store/upload/forms/Colors.js
+++ src/pages/admin/store/upload/forms/Colors.js
@@ -1,57 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { sizes, categories } from "../data";
 
-const Colors = ({ payloadChange }) => {
-  const [numOfInputs, setNumOfInputs] = useState(0);
-  const Form = () => (
-    <div className="w-full h-full flex flex-col items-center justify-start">
-      <div className="w-32">
-        <label className="">Color:</label>
-        <input
-          key="Color"
-          type="text"
-          name="Color"
-          className="bg-white shadow-md border-gray-300 rounded w-32"
-          style={{ borderWidth: "1px" }}
-          onChange={payloadChange}
-          placeholder="0.00"
-        ></input>
-      </div>
+const Colors = ({ payloadChange, payload, setPayload }) => {
+  const [sizes, setSizes] = useState([
+    { size: "xs", quantity: undefined },
+    { size: "sm", quantity: undefined },
+    { size: "md", quantity: undefined },
+    { size: "lg", quantity: undefined },
+    { size: "xl", quantity: undefined },
+    { size: "2xl", quantity: undefined },
+  ]);
 
-      <div className="flex w-full items-center ">
-        <p className="text-xl font-semibold w-1/3 text-left ">Sizes:</p>
-        <select
-          name="size"
-          onChange={(e) => payloadChange(e)}
-          value={payload.sizes}
-          className="pl-2 sm:pl-4 shadow-md border-faded-gray rounded-md  h-12 w-3/5 self-end"
-          style={{ borderWidth: "1px" }}
-        >
-          {sizes?.map((item, i) => (
-            <option key={i} value={item}>
-              {item}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="w-32">
-        <label className="">Quantity:</label>
-        <input
-          key="quantity"
-          type="text"
-          name="quantity"
-          className="bg-white shadow-md border-gray-300 rounded w-32"
-          style={{ borderWidth: "1px" }}
-          onChange={payloadChange}
-          placeholder="0.00"
-        ></input>
-      </div>
-    </div>
-  );
+  function handleChange(index, event) {
+    let data = [...sizes];
+    data[index][event.target.name] = event.target.value;
+    setSizes(data);
+  }
+
+  useEffect(() => {
+    setPayload({
+      ...payload,
+      sizes: sizes,
+    });
+  }, [sizes]);
+
   return (
-    <div className="w-full h-full flex flex-col items-center justify-start">
-      <p>Please add colors, sizes, and quantities available for this product</p>
-      <div className=""></div>
+    <div className="w-full h-full flex flex-col items-center justify-start px-4">
+      <p className="mb-8">
+        Please sizes and quantities available for this product
+      </p>
+
+      <div className="w-full h-full flex flex-wrap justify-between items-start">
+        {sizes.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-row items-center justify-start w-2/5 space-x-2 h-32"
+            >
+              <p className="w-1/3">{item.size}</p>
+              <input
+                //type={"number"}
+                className="h-10 w-full px-2 rounded-md border-faded-light shadow-md"
+                placeholder="Quantity"
+                name="quantity"
+                onChange={(event) => handleChange(index, event)}
+              ></input>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
