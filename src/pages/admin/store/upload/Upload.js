8c2214688be52d78e7e26dd83544b61eca659e78
anthonycoded <barbels33@gmail.com>
2022-06-23 13:12:25 -0400

    updated layout, logic, payload

--- src/pages/admin/store/upload/Upload.js
+++ src/pages/admin/store/upload/Upload.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BounceLoader, FadeLoader } from "react-spinners";
 
-import Info from "./uploadFormSteps/Info";
+import Info from "./forms/Info";
+import Confirmation from "./forms/Confirmation";
+import Colors from "./forms/Colors";
 
 import {
   PublishProduct,
@@ -12,9 +14,11 @@ import {
 } from "../../../../store/actions/productActions";
 
 const Upload = ({ open, setOpen }) => {
+  const state = useSelector((state) => state.products);
   const [imageFile, setImageFile] = useState([]);
   const [image, setImage] = useState([]);
-  const state = useSelector((state) => state.products);
+  const [step, setStep] = useState(0);
+
   const dispatch = useDispatch();
 
   const [payload, setPayload] = useState({
@@ -23,7 +27,8 @@ const Upload = ({ open, setOpen }) => {
     price: undefined,
     description: undefined,
     featured: false,
-    colors: [{ size: "", qty: undefined, color: undefined }], //Ex. {size: "xs", qty: 10, color:"aqua/southbeach"}
+    color: undefined, //Ex. {size: "xs", qty: 10, color:"aqua/southbeach"}
+    sizes: [],
     category: undefined,
     tags: [],
   });
@@ -38,7 +43,7 @@ const Upload = ({ open, setOpen }) => {
   });
 
   const payloadChange = (e) => {
-    e.preventDefault();
+    //e.preventDefault();
     setPayload({
       ...payload,
       [e.target.name]: e.target.value,
@@ -50,6 +55,7 @@ const Upload = ({ open, setOpen }) => {
       setStatus({
         ...status,
         loading: true,
+        publishing: true,
         message: "Publishing Product",
       });
       dispatch(PublishProduct(payload));
@@ -79,11 +85,13 @@ const Upload = ({ open, setOpen }) => {
         colors: {},
       });
     }
-  }, [products]);
+  }, [state]);
+
+  console.log(payload);
 
   const Buttons = () => (
     <div>
-      {step === 1 ? (
+      {step == 0 ? (
         <div className="flex justify-end space-x-6">
           <button
             onClick={() => setOpen(false)}
@@ -93,7 +101,7 @@ const Upload = ({ open, setOpen }) => {
           </button>
           <button
             onClick={() => {
-              setStep(2);
+              setStep(step + 1);
             }}
             type="button"
             className="h-12 w-32 rounded-md border disabled:opacity-50 bg-green-400  shadow-sm px-4 py-2  hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
@@ -101,17 +109,17 @@ const Upload = ({ open, setOpen }) => {
             Next
           </button>
         </div>
-      ) : step === 2 ? (
+      ) : step > 0 && step < 2 ? (
         <div className="flex justify-end space-x-6">
           <button
-            onClick={() => setStep(1)}
+            onClick={() => setStep(step - 1)}
             type="button"
             className="w-32 h-12 font-medium rounded-md shadow-sm bg-yellow-400  text-gray-500 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
           >
             Back
           </button>
           <button
-            onClick={() => setStep(3)}
+            onClick={() => setStep(step + 1)}
             type="button"
             className="h-12 w-32 bg-green-400 font-medium rounded-md border border-gray-300 shadow-sm px-4 py-2 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
           >
@@ -121,14 +129,14 @@ const Upload = ({ open, setOpen }) => {
       ) : (
         <div className="flex justify-end space-x-6">
           <button
-            onClick={() => setStep(2)}
+            onClick={() => setStep(step - 1)}
             type="button"
             className="w-32 h-12  rounded-md shadow-sm font-medium bg-yellow-400  text-white hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500 sm:ml-3 sm:w-auto sm:text-sm"
           >
             Back
           </button>
           <button
-            onClick={publishSong}
+            onClick={publish}
             type="button"
             className="h-12 w-32 rounded-md border shadow-sm font-medium  bg-green-400 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
           >
@@ -138,6 +146,40 @@ const Upload = ({ open, setOpen }) => {
       )}
     </div>
   );
+
+  //Switch board function to navigate from each page//
+  function formStep(step) {
+    switch (step) {
+      case 0:
+        return (
+          <Info
+            payload={payload}
+            setPayload={setPayload}
+            payloadChange={payloadChange}
+          />
+        );
+      case 1:
+        if (payload.category == "Shirts") {
+          return (
+            <Colors
+              payload={payload}
+              payloadChange={payloadChange}
+              setPayload={setPayload}
+            />
+          );
+        } else {
+          return (
+            <Colors
+              payload={payload}
+              payloadChange={payloadChange}
+              setPayload={setPayload}
+            />
+          );
+        }
+      case 2:
+        return <Confirmation payload={payload} payloadChange={payloadChange} />;
+    }
+  }
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
@@ -154,38 +196,25 @@ const Upload = ({ open, setOpen }) => {
         </span> */}
 
         <div
-          className="flex flex-col relative  justify-between h-full w-full bg-white p-4 rounded-lg text-left shadow-xl sm:my-8  sm:max-w-lg sm:w-full"
+          className="flex flex-col relative  justify-between h-130 w-full bg-white py-2 rounded-lg text-left shadow-xl sm:my-2  sm:max-w-lg sm:w-full"
           role="dialog"
           aria-modal="true"
           aria-labelledby="modal-headline"
         >
-          <h3 className="text-2xl font-bold leading-6  text-gray-900 text-center mb-8">
-            Upload Song
+          <h3 className="text-xl font-semibold text-primary-main text-center mb-3">
+            Add product to store
           </h3>
           {status.loading ? (
             <div className="w-full h-60 items-center justify-center flex flex-col">
-              <FadeLoader loading={publishing} color="blue"></FadeLoader>
+              <FadeLoader loading={status.publishing} color="blue"></FadeLoader>
               <p className="text-xl font-semibold  capitalize text-primary">
                 Publishing product to store...
               </p>
             </div>
           ) : (
-            <div className="w-full h-full">
-              {step == 0 ? (
-                <Info
-                  payloadChange={payloadChange}
-                  setImageFile={setImageFile}
-                  setImage={setImage}
-                ></Info>
-              ) : (
-                <Confirmation
-                  payload={payload}
-                  imageFile={imageFile}
-                ></Confirmation>
-              )}
-            </div>
+            <div className="h-full w-full">{formStep(step)}</div>
           )}
-          <div>
+          <div className="px-4">
             <Buttons></Buttons>
           </div>
         </div>
